b5744312e4d9fa4e61e841047e1a832b22bca2d4
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2025-02-10 22:30:04 -0700

    Added functionality to process data

--- stock_analysis.py
+++ stock_analysis.py
@@ -36,8 +36,57 @@ def download_data(ticker: str) -> dict:
 
     return response
 
+def process_data(dataset: dict) -> dict:
+    """Calculates and returns statistics for closing prices from the dataset
+
+    Argument:
+        dataset (dict): dictionary to get the statistics from
+    
+    Returns:
+        Dict containing min, max, average, and median of the closing prices in the provided dataset
+    """
+
+    # create list of closing prices for each day in dataset
+    closing_data = [float(day['close'][1:]) for day in dataset['data']['tradesTable']['rows']]
+
+    # calculate each statistic
+    max_closing = max(closing_data)
+    min_closing = min(closing_data)
+    avg_closing = sum(closing_data) / len(closing_data)
+    med_closing = median(closing_data)
+
+    # turn into dict and return
+    return {"max": max_closing, "min": min_closing, "avg": avg_closing, "median": med_closing}     
+        
+def median(input: list[float]) -> float:
+    """Calculates the median value of a list
+    
+    Arguments:
+        input (list of floats): the list of floats to find the median of
+
+    Returns:
+        Float containing the median value of the list
+    """
+
+    # get size
+    input_size = len(input)
+
+    # get half size (cast to int)
+    mid_index = int(input_size / 2)
+
+    # sort values
+    input.sort()
+    
+    # if there is a middle value, return it
+    if input_size % 2 != 0:
+        return input[mid_index]
+    
+    # if there is not a middle value, return the average of the two middle values
+    return (input[mid_index] + input[mid_index + 1]) / 2
+
+
 if __name__ == "__main__":
     ticker = "LMT"
-    d = download_data(ticker)
-    for row in d['data']['tradesTable']['rows']:
-        print(f"On {row['date']}, {ticker} opened at {row['open']}")
+    data = download_data(ticker)
+    closing_stats = process_data(data)
+    print(closing_stats)
